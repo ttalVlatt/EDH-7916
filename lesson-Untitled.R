b7b7364947e46a59537c6ee4e47b44e6e4526343
ttalVlatt <matt.capaldi@outlook.com>
2025-02-26 19:12:06 -0500

    update scripts

--- lesson-Untitled.R
+++ lesson-Untitled.R
@@ -1,1 +1,15 @@
+1+1
 
+library(tidyverse)
+library(haven)
+
+data <- read_dta("data/hsls-small.dta")
+
+ggplot(data) +
+  geom_histogram(aes(x = x1txmtscor,
+                     fill = as_factor(x1sex)),
+                 alpha = 0.5) +
+  labs(x = "Math Score",
+       fill = "Sex") +
+  theme_minimal() +
+  scale_fill_viridis_d(option = "Magma")
